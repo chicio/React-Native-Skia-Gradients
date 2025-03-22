32d8f70b524af7126aa3c30e7c5bfa20f4e1d09e
chicio <fabrizio.duroni@gmail.com>
2025-03-22 11:46:08 +0100

    Prepared tab layout.

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,23 @@
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { Tabs } from 'expo-router';
+
+export default function TabLayout() {
+    return (
+        <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }}>
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: 'Texts',
+                    tabBarIcon: ({ color }) => <FontAwesome name="file-text" size={24} color={color} />,
+                }}
+            />
+            <Tabs.Screen
+                name="paths"
+                options={{
+                    title: 'Paths',
+                    tabBarIcon: ({ color }) => <FontAwesome name="paint-brush" size={24} color={color} />,
+                }}
+            />
+        </Tabs>
+    );
+}
