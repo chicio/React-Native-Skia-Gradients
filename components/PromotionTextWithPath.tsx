f0c8c61bb9841265a1dd8c044fb87af6ed094343
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2025-04-06 13:16:54 +0200

    Updated component with latest improvement original codebase :rocket:

--- components/PromotionTextWithPath.tsx
+++ components/PromotionTextWithPath.tsx
@@ -1,63 +1,79 @@
 import React from 'react';
 import {Canvas, LinearGradient, Mask, Path, Rect, vec,} from '@shopify/react-native-skia';
-import {LayoutChangeEvent, StyleSheet, Text, View, ViewStyle,} from 'react-native';
+import {LayoutChangeEvent, StyleSheet, Text, View,} from 'react-native';
 
-const underlineSvg =
+const SVG_UNDERLINE =
     'M1.83778 3C1.07806 3 0.407248 2.69942 0.253688 2.26222C0.0839635 1.77037 0.649712 1.29218 1.53066 1.19199C2.43586 1.09179 23.9909 -1.2354 46.5642 0.895963C47.4452 0.977939 48.0433 1.44702 47.8897 1.94343C47.7442 2.43983 46.9118 2.77229 46.0308 2.69032C24.0475 0.622712 2.36312 2.95901 2.1449 2.98178C2.03984 2.99545 1.93477 3 1.8297 3H1.83778Z';
-const underlineBaseWidth = 48;
-const underlineBaseHeight = 3;
-const underlineMultiplierFactor = 1.1;
-const padding = 5;
 
-interface UnderlineConfig {
-    scalingFactor: number;
+const BASE_UNDERLINE_WIDTH = 48;
+const BASE_UNDERLINE_HEIGHT = 3;
+const BASE_TEXT_LINE_HEIGHT = 14;
+const UNDERLINE_MULTIPLIER_FACTOR = 1.1;
+const TOP_POSITION_ADDITIONAL_PADDING = 0.2;
+
+type UnderlineConfig = {
     width: number;
     height: number;
-}
+    scalingFactorWidth: number;
+    scalingFactorHeight: number;
+    topPosition: number;
+};
 
-interface LastMinuteDealsPromotionBadgeProps {
-    style?: ViewStyle;
-}
-
-export const PromotionTextWithPath: React.FC<
-    LastMinuteDealsPromotionBadgeProps
-> = ({style}) => {
-    const [underlineConfig, setUnderlineConfig] = React.useState<UnderlineConfig | null>(null);
+export const PromotionTextWithPath: React.FC = () => {
+    const [underlineConfig, setUnderlineConfig] = React.useState<UnderlineConfig | undefined>(undefined);
 
     const onDealsTextLayout = (event: LayoutChangeEvent) => {
-        const {width} = event.nativeEvent.layout;
-        const scalingFactor =
-            (width / underlineBaseWidth) * underlineMultiplierFactor;
-        const underlineWidth = width * underlineMultiplierFactor;
-        const underlineHeight = underlineBaseHeight * scalingFactor;
+        const {width, height} = event.nativeEvent.layout;
+
+        const underlineWidth = width * UNDERLINE_MULTIPLIER_FACTOR;
+        const underlineHeight = (BASE_UNDERLINE_HEIGHT * styles.deals.fontSize) / BASE_TEXT_LINE_HEIGHT;
+
+        const scalingFactorWidth = (width / BASE_UNDERLINE_WIDTH) * UNDERLINE_MULTIPLIER_FACTOR;
+        const scalingFactorHeight = underlineHeight / BASE_UNDERLINE_HEIGHT;
+
+        const paddingBottom = (height - styles.deals.fontSize) / 2;
+        const topPosition = height - paddingBottom + paddingBottom * TOP_POSITION_ADDITIONAL_PADDING;
 
         setUnderlineConfig({
-            scalingFactor,
             width: underlineWidth,
             height: underlineHeight,
+            scalingFactorWidth,
+            scalingFactorHeight,
+            topPosition,
         });
     };
 
     return (
-        <View style={{...styles.container, ...style}}>
-            <Text style={styles.deals} onLayout={onDealsTextLayout}>
+        <View style={styles.container}>
+            <Text
+                style={styles.deals}
+                onLayout={onDealsTextLayout}
+                numberOfLines={1}>
                 DEALS
             </Text>
-            {
-                underlineConfig && <Canvas style={{
-                    width: underlineConfig.width,
-                    height: underlineConfig.height + padding,
-                    position: 'absolute',
-                    bottom: -padding,
-                    left: -5,
-                }}>
+            {underlineConfig && (
+                <Canvas
+                    style={{
+                        width: Math.ceil(underlineConfig.width),
+                        height: Math.ceil(underlineConfig.height),
+                        position: 'absolute',
+                        top: underlineConfig.topPosition,
+                        right: -(underlineConfig.width * 0.05),
+                    }}>
                     <Mask
                         mode={'alpha'}
                         mask={
                             <Path
-                                path={underlineSvg}
-                                color={'#EAEAEB'}
-                                transform={[{scale: underlineConfig.scalingFactor}]}
+                                path={SVG_UNDERLINE}
+                                color={"#000000"}
+                                transform={[
+                                    {
+                                        scaleX: underlineConfig.scalingFactorWidth,
+                                    },
+                                    {
+                                        scaleY: underlineConfig.scalingFactorHeight,
+                                    },
+                                ]}
                             />
                         }>
                         <Rect
@@ -66,16 +82,14 @@ export const PromotionTextWithPath: React.FC<
                             width={underlineConfig.width}
                             height={underlineConfig.height}>
                             <LinearGradient
-                                start={vec(0, underlineBaseHeight / 2)}
-                                end={vec(underlineConfig.width, underlineBaseHeight / 2)}
-                                colors={[
-                                    '#F2007C', '#F7AF17'
-                                ]}
+                                start={vec(0, underlineConfig.height / 2)}
+                                end={vec(underlineConfig.width, underlineConfig.height / 2)}
+                                colors={['#F2007C', '#F7AF17']}
                             />
                         </Rect>
                     </Mask>
                 </Canvas>
-            }
+            )}
         </View>
     );
 };
@@ -83,10 +97,7 @@ export const PromotionTextWithPath: React.FC<
 const styles = StyleSheet.create({
     container: {
         flexDirection: 'row',
-        justifyContent: 'flex-end',
         alignItems: 'center',
-        marginBottom: 2,
-        overflow: "visible"
     },
     deals: {
         fontSize: 48,
